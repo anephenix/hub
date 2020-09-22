05b678d3aebed329072d8126f9bee0803150bf2e
Paul Jensen <paulbjensen@gmail.com>
2020-09-22 09:19:55 +0100

    Added tests for the message parsing

--- __tests__/lib/messageParsing.test.js
+++ __tests__/lib/messageParsing.test.js
@@ -1,31 +1,27 @@
-// const { parseMessage } = require('../../lib/messageParsing');
-
-// const parseMessage = ({ message, ws }) => {
-// 	try {
-// 		const payload = JSON.parse(message);
-// 		if (payload.action) {
-// 			if (payload.action === 'reply-client-id') {
-// 				console.log('Received reply');
-// 				processClientId(payload.data, ws);
-// 			}
-// 		} else {
-// 			console.log('received: %s', message);
-// 		}
-// 	} catch (err) {
-// 		console.log('Error parsing message received from client');
-// 		console.error(err);
-// 	}
-// };
+// Dependencies
+const assert = require('assert');
+const { parseMessage } = require('../../lib/messageParsing');
 
 describe('messageParsing', () => {
 	describe('#parseMessage', () => {
 		describe('when it does not receive a valid JSON payload', () => {
-			it.todo('should return an error');
+			it('should return an error', () => {
+				const ws = {};
+				const message = '20fh02nd0n10i0i1ns0n10n010d-1d-1-d-1nd-1-dn'; // Not JSON
+				const response = parseMessage({ ws, message });
+				assert.ifError(response);
+			});
 		});
 		describe('when it receives a valid JSON payload', () => {
-			it.todo(
-				'should parse the payload, and action it if the payload is a reply for a client id request'
-			);
+			it('should parse the payload, and action it if the payload is a reply for a client id request', () => {
+				const ws = {};
+				const message = JSON.stringify({
+					action: 'reply-client-id',
+					data: { clientId: 'abc' },
+				});
+				parseMessage({ ws, message });
+				assert(ws.clientId === 'abc');
+			});
 		});
 	});
 });
