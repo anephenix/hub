74891223c6da8d083ea023c241a40c86a4f3aeee
Paul Jensen <paulbjensen@gmail.com>
2020-09-28 16:15:16 +0100

    Added some more unit tests for pubsub

--- __tests__/lib/pubsub.test.js
+++ __tests__/lib/pubsub.test.js
@@ -23,21 +23,56 @@ describe('pubsub', () => {
 		});
 
 		describe('when the websocket does not have a client id', () => {
-			it.todo(
-				'should return an error response indicating that the websocket does not have an id'
-			);
+			it('should return an error response indicating that the websocket does not have an id', () => {
+				let sentPayload = null;
+				const data = { channel: 'weather' };
+				const ws = {
+					send: (payload) => (sentPayload = JSON.parse(payload)),
+				};
+				pubsub.subscribe({ data, ws });
+				assert.strictEqual(sentPayload.success, false);
+				assert.strictEqual(
+					sentPayload.message,
+					'No client id was found on the WebSocket'
+				);
+			});
 		});
 
 		describe('when the channel is not passed', () => {
-			it.todo(
-				'should return an error response indicating that the channel was not passed'
-			);
+			it('should return an error response indicating that the channel was not passed', () => {
+				let sentPayload = null;
+				const data = {};
+				const ws = {
+					clientId: 'yyyy',
+					send: (payload) => (sentPayload = JSON.parse(payload)),
+				};
+				pubsub.subscribe({ data, ws });
+				assert.strictEqual(sentPayload.success, false);
+				assert.strictEqual(
+					sentPayload.message,
+					'No channel was passed in the data'
+				);
+			});
 		});
 
 		describe('when a client makes multiple attempts to subscribe to the same channel', () => {
-			it.todo(
-				'should only record a single entry of the client id in the channel subscriptions, and vice versa'
-			);
+			it('should only record a single entry of the client id in the channel subscriptions, and vice versa', () => {
+				let sentPayload = null;
+				const data = { channel: 'entertainment' };
+				const ws = {
+					clientId: 'zzzz',
+					send: (payload) => (sentPayload = JSON.parse(payload)),
+				};
+				pubsub.subscribe({ data, ws });
+				pubsub.subscribe({ data, ws });
+				assert(sentPayload.success);
+				assert.strictEqual(
+					sentPayload.message,
+					'Client "zzzz" subscribed to channel "entertainment"'
+				);
+				assert.deepStrictEqual(pubsub.channels.entertainment, ['zzzz']);
+				assert.deepStrictEqual(pubsub.clients.zzzz, ['entertainment']);
+			});
 		});
 	});
 });
