1c2cd4f6d74f0327e7482b98fffcf28cd28a810a
Paul Jensen <paulbjensen@gmail.com>
2022-04-16 09:25:03 +0100

    Added memory checker rpc example

---
+++ examples/rpc/memory-report/client.js
@@ -0,0 +1,11 @@
+const HubClient = require('../../../lib/client');
+const hubClient = new HubClient({ url: 'ws://localhost:6000' });
+const os = require('os');
+
+const reportMemory = ({ reply }) => {
+	const freemem = os.freemem();
+	const totalmem = os.totalmem();
+	reply({ data: { freemem, totalmem } });
+};
+
+hubClient.rpc.add('report-memory', reportMemory);
